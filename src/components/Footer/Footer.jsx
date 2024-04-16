5440466ffada84bb8ef36fe5eb379b4d5cb86d12
AlsuGai <alsu.gai05@mail.ru>
2024-04-16 21:29:32 +0300

    create some components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <nav className="footer">
+            <div>Logo</div>
+            <a href="">Найти слово</a>
+            <a href="">Темы</a>
+            <a href="">О нас</a>
+        </nav>
+    )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <nav className="header">
+            <div>Logo</div>
+            <a href="">Найти слово</a>
+            <a href="">Темы</a>
+            <a href="">О нас</a>
+        </nav>
+    )
+}
