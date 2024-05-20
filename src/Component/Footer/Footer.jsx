97f131a4c641fd6c8a57173e437e37e9eb195030
AlsuGai <alsu.gai05@mail.ru>
2024-05-20 17:25:52 +0300

    img

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 export default function Footer() {
     return (
         <nav className="footer">
-            <NavLink to='/learn-language'><img className="footer__logo" src="/src/assets/images/logo.png" alt="LearnEnglish" /></NavLink>
+            <NavLink to='/learn-language'><img className="footer__logo" src="/learn-language/src/assets/images/logo.png" alt="LearnEnglish" /></NavLink>
             <div className='footer__buttons'>
             <NavLink to="/cards">Карточки</NavLink>
             <NavLink to="/themes">Темы</NavLink>
--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 export default function Header() {
     return (
         <nav className="header">
-            <NavLink to='/learn-language'><img className="header__logo" src="/src/assets/images/logo.png" alt="LearnEnglish" /></NavLink>
+            <NavLink to='/learn-language'><img className="header__logo" src="src/assets/images/logo.png" alt="LearnEnglish" /></NavLink>
             <div className="header__buttons">
             <NavLink to="/cards">Карточки</NavLink>
             <NavLink to="/themes">Темы</NavLink>
